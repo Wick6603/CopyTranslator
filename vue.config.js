91cc234aa2c3914fd28c3c0b849355e3c7d66cf7
Wick6603 <156735781+Wick6603@users.noreply.github.com>
2025-11-21 22:43:07 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -58,51 +58,13 @@ module.exports = {
           target: [
             {
               target: "nsis",
-              arch: ["x64"],
+              arch: ["arm64"],
             },
             {
               target: "zip",
-              arch: ["x64"],
+              arch: ["arm64"],
             },
           ],
-        },
-        linux: {
-          target: [
-            {
-              target: "AppImage",
-              arch: ["x64"],
-            },
-            {
-              target: "deb",
-              arch: ["x64"],
-            },
-            {
-              target: "rpm",
-              arch: ["x64"],
-            },
-          ],
-          maintainer: "ziqiang_xu@qq.com",
-          icon: osSpec.iconName,
-          category: "Education",
-          // https://www.electron.build/configuration/linux#debian-package-options
-          desktop: {
-            Icon: "/opt/copytranslator/resources/linux-icon/icon.png",
-          },
-        },
-        deb: {
-          depends: ["libpng16-16"],
-        },
-        rpm: {
-          depends: ["libpng"],
-        },
-        mac: {
-          target: [
-            {
-              target: "default",
-              arch: ["x64"],
-            },
-          ],
-          icon: osSpec.iconName,
         },
         nsis: {
           installerIcon: osSpec.iconName,
